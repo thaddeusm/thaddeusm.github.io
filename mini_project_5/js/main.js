11109183793a846ed0746cf656f9f8a2026d603f
Thaddeus McCleary <imac@Thaddeuss-iMac.local>
2017-11-11 23:58:20 -0600

    reverting event listeners

--- mini_project_5/js/main.js
+++ mini_project_5/js/main.js
@@ -107,20 +107,7 @@ var controller = {
 			}
 
 			console.log(e);
-			if (e.target.tagName != 'img') {
-				view.emptyElement('#centeredImage');
-				view.hideElement('#lightbox');
-				view.displayElement('.container');
-			}
-		});
-
-		window.addEventListener('touchstart', function(e) {
-			if (e.target.id != 'create' && e.target.id != 'searchButton' && e.target.id != 'randomButton') {
-				view.hideElement('#dropdownContent');
-			}
-
-			console.log(e);
-			if (e.target.tagName != 'img') {
+			if (e.target.id == 'lightbox') {
 				view.emptyElement('#centeredImage');
 				view.hideElement('#lightbox');
 				view.displayElement('.container');
