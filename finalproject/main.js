f861669634d4248e011d628248688e1412da7a40
Thaddeus McCleary <imac@Thaddeuss-iMac.local>
2017-12-05 15:41:06 -0600

    final testing

--- finalproject/main.js
+++ finalproject/main.js
@@ -1,7 +1,12 @@
+// model for the single-page application
 var presentation = {
+	// this array will be populated by data from the Google Sheet
 	data: [],
+	// this array will be populated by two database items after shuffling
 	choices: [],
+	// this property will store the user's keyword choice
 	choice: '',
+	// this method shuffles the data returned by Tabletop to gamify the application
 	shuffleData: function() {
 		var i = 0
 	      , j = 0
@@ -16,16 +21,21 @@ var presentation = {
 
 	  this.pushChoices();
 	},
+	// after shuffling, this method pulls the first two items out of the array and
+	// presents them to the user for the final decision
 	pushChoices: function() {
 		for (let i=0; i<2; i++) {
 			this.choices.push(this.data[i]);
 		}
 		console.log(this.choices);
+		// choices are displayed to the user
 		view.printChoices();
 	}
 };
 
+// controller object to handle application interactions and transitions
 var controller = {
+	// sets event listeners upon page load
 	startListeners: function() {
 		var startButton = document.getElementById('startButton');
 
@@ -37,13 +47,16 @@ var controller = {
 			choiceScreen.style.display = 'grid';
 		});
 	},
+	// triggers Reveal JS once the user has chosen a keyword
 	startPresentation: function(choice) {
 		presentation.choice = presentation.choices[choice];
 		view.printSlideContent();
 	},
+	// this method is called after Tabletop JS retrieves the database from Google Sheets
 	loadOptions: function() {
 		presentation.shuffleData();
 	},
+	// this method initializes Reveal JS once slide content has been added to the DOM
 	initSlides: function() {
 		Reveal.initialize({
 			dependencies: [
@@ -54,12 +67,15 @@ var controller = {
 			]
 		});
 	},
+	// this restarts the application for a second round
 	restart: function() {
 		location.reload(true);
 	}
 };
 
+// the view object to control feedback and visible transitions
 var view = {
+	// this method displays the keyword choices to the user
 	printChoices: function() {
 		var first = document.getElementById('firstChoice');
 		var second = document.getElementById('secondChoice');
@@ -77,6 +93,8 @@ var view = {
 		first.appendChild(firstButton);
 		second.appendChild(secondButton);
 	},
+	// this method pushes slide content to the DOM from the
+	// Google Sheet that Tabletop JS connected
 	printSlideContent: function() {
 		var slides = document.querySelector('.slides');
 
@@ -103,20 +121,23 @@ var view = {
 		var restartButton = '<h3>Restart</h3><br><br><button class="restart-button" type="button" onclick="controller.restart()"><img src="media/return.svg" alt="return icon" class="link-icon"></button>';
 		var restartSlide = this.buildSection(restartButton);
 
+		// after slides are built they are pushed into the container required by Reveal JS
 		slides.appendChild(photoSlide);
 		slides.appendChild(quoteSlide);
 		slides.appendChild(promptSlide);
 		slides.appendChild(resourceSlide);
 		slides.appendChild(restartSlide);
 
+		// this manual delay helps ensure that content is fully loaded before the presentation starts
 		setTimeout(function() {
 			controller.initSlides();
-		}, 500);
+		}, 700);
 
 		this.hideElement('#keywordChoice');
 		this.displayElement('.reveal');
 
 	},
+	// this is a reusable function that displays DOM elements
 	displayElement: function(element) {
 		if (arguments[1]) {
 			document.querySelector(element).style.display = 'grid';
@@ -124,9 +145,11 @@ var view = {
 			document.querySelector(element).style.display = 'block';
 		}
 	},
+	// this is a reusable function that hides DOM elements
 	hideElement: function(element) {
 		document.querySelector(element).style.display = 'none';
 	},
+	// this is a reusable function that builds a DOM button element
 	buildButton: function(content, onclick) {
 		let button = document.createElement('button');
 
@@ -137,6 +160,7 @@ var view = {
 
 		return button;
 	},
+	// this is a resuable function that builds a DOM section element
 	buildSection: function(content) {
 		let section = document.createElement('section');
 		section.innerHTML = content;
@@ -145,9 +169,12 @@ var view = {
 	}
 };
 
+// initiates Tabletop JS to pull in data from Google Sheets
 function init() {
+	// the public URL to my Google Sheet is passed into Tabletop
 	Tabletop.init( { key: 'https://docs.google.com/spreadsheets/d/1mtqTFGXCJbnX15p3WYYY8h4MIg9G-UiEwNy16MX5CJ4/edit?usp=sharing',
 	callback: function(data, tabletop) { 
+	   // once the connection is complete, data from the spreadsheet is loaded into the application
 	   for (var i=0; i<data.length; i++) {
 	   		if (data[i]['Additional Resources'].length > 1 && data[i]['Characteristic'].length > 1 && data[i]['Image Link'].length > 1 && data[i]['Personal Prompt'].length > 1 && data[i]['Quote'].length > 1) {
 	   			presentation.data.push(data[i]);
@@ -157,6 +184,8 @@ function init() {
 	},
 	simpleSheet: true } )
 }
+// starts Tabletop only after the page is fully loaded
 window.addEventListener('DOMContentLoaded', init);
 
+// starts event listeners
 controller.startListeners();
